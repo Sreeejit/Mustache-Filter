fc7cffa8a80a2c82f1877b0bd82e1c185d183cf1
Sreeejit <77166984+Sreeejit@users.noreply.github.com>
2021-09-21 19:10:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+canvas = createCanvas(300, 300)
+canvas.position(615, 200)
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+save('realtimeFilter.png')
+}
